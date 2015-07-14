1f9da210d627e7ee74832719c29df829ab725671
Jasmi <jasmi789@gmail.com>
2015-07-14 16:01:00 +0530

    home fragment changes

--- app/src/main/java/in/gravitykerala/universityofcalicut/Fragment/DemoFragment.java
+++ app/src/main/java/in/gravitykerala/universityofcalicut/Fragment/DemoFragment.java
@@ -1,11 +1,14 @@
 package in.gravitykerala.universityofcalicut.Fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import in.gravitykerala.universityofcalicut.R;
 
@@ -15,6 +18,8 @@ import in.gravitykerala.universityofcalicut.R;
  */
 public class DemoFragment extends Fragment {
     private static final String KEY_TITLE = "title";
+    LinearLayout news, uni_orders;
+
 
     public DemoFragment() {
         // Required empty public constructor
@@ -35,6 +40,20 @@ public class DemoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         // don't look at this layout it's just a listView to show how to handle the keyboard
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        news = (LinearLayout) v.findViewById(R.id.news);
+        uni_orders = (LinearLayout) v.findViewById(R.id.uni_orders);
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.cuonline.ac.in/?cat=5"));
+                startActivity(intent);
+            }
+        });
+        return v;
+
     }
 }
