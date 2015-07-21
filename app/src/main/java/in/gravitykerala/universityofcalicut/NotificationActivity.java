fecae77f43a1aad7a194bc4e0844a1d14620ce9f
Prakash <expect112@gmail.com>
2015-07-21 23:10:20 +0530

    Added Notification Tag retrieval system

--- app/src/main/java/in/gravitykerala/universityofcalicut/NotificationActivity.java
+++ app/src/main/java/in/gravitykerala/universityofcalicut/NotificationActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
--- app/src/main/java/in/gravitykerala/universityofcalicut/PushNotificationHandler.java
+++ app/src/main/java/in/gravitykerala/universityofcalicut/PushNotificationHandler.java
@@ -12,10 +12,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.microsoft.windowsazure.notifications.NotificationsHandler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
 public class PushNotificationHandler extends NotificationsHandler implements GravitySupport {
 
@@ -82,6 +91,42 @@ public class PushNotificationHandler extends NotificationsHandler implements Gra
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+    String[] getSubscribedNotifications(Context context) {
+        Set<String> subscribedNotiificationsList = new HashSet<String>();
+        prefs = context.getSharedPreferences(KEY_PREFERENCE_ID, Context.MODE_APPEND);
+        String selectedCourse = prefs.getString(KEY_SELECTED_COURSE_ID, "none");
+        String selectedDepartment = prefs.getString(KEY_SELECTED_DEPARTMENT_ID, "none");
+        if (!selectedCourse.equals("none")) {
+            subscribedNotiificationsList.add(TAG_COMMON_EXAM + selectedCourse);
+            subscribedNotiificationsList.add(TAG_COMMON_EXAM + selectedDepartment);
+
+            subscribedNotiificationsList.add(TAG_NOTIFICATION_EXAM_NOTIFICATIONS);
+            subscribedNotiificationsList.add(TAG_NOTIFICATION_EXAM_RESULT);
+            subscribedNotiificationsList.add(TAG_NOTIFICATION_EXAM_TIMETABLE);
+
+            Boolean distanceEducationNeeded = prefs.getBoolean(KEY_DISTANCE_EDUCATION_NEEDED, false);
+            if (distanceEducationNeeded) {
+                subscribedNotiificationsList.add(TAG_COMMON_DISTANCE + selectedCourse);
+                subscribedNotiificationsList.add(TAG_COMMON_DISTANCE + selectedDepartment);
+
+                subscribedNotiificationsList.add(TAG_NOTIFICATION_DISTANCE_CONTACT_CLASS);
+                subscribedNotiificationsList.add(TAG_NOTIFICATION_DISTANCE_NOTIFICATIONS);
+                subscribedNotiificationsList.add(TAG_NOTIFICATION_DISTANCE_QUESTION_BANK);
+                subscribedNotiificationsList.add(TAG_NOTIFICATION_DISTANCE_STUDY_MATERIAL);
+            }
+        }
+        if (prefs.getBoolean(KEY_VC_DESK_NEEDED, false)) {
+            subscribedNotiificationsList.add(TAG_NOTIFICATION_VC_DESK);
+        }
+        if (prefs.getBoolean(KEY_UOC_NEWS_NEEDED, false)) {
+            subscribedNotiificationsList.add(TAG_NOTIFICATION_NEWS);
+        }
+        if (prefs.getBoolean(KEY_UOC_ORDERS_NEEDED, false)) {
+            subscribedNotiificationsList.add(TAG_NOTIFICATION_ORDERS);
+        }
+        return subscribedNotiificationsList.toArray(new String[subscribedNotiificationsList.size()]);
     }
 
 }
