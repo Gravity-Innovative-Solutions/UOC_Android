092c8512b51da8624230ebbbac668dba4453059a
Jasmi <jasmi789@gmail.com>
2015-07-26 11:41:32 +0530

    Ripple effect added in Distance Education Talk to VC connected to Server Link phone calls in Administrative Numbers

--- app/src/main/java/in/gravitykerala/universityofcalicut/AdministrativeNumbers.java
+++ app/src/main/java/in/gravitykerala/universityofcalicut/AdministrativeNumbers.java
@@ -1,17 +1,22 @@
 package in.gravitykerala.universityofcalicut;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
 public class AdministrativeNumbers extends AppCompatActivity {
 
-    private static final int PHONE_NUMBER_MINIMUM_DIGITS = 5;
+    private static final int PHONE_NUMBER_MINIMUM_DIGITS = 11;
 
 
 
@@ -21,6 +26,52 @@ public class AdministrativeNumbers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrative_numbers);
         TextView myPhone = (TextView) findViewById(R.id.textView71);
+        TextView myPhone1 = (TextView) findViewById(R.id.textView73);
+        TextView myPhone2 = (TextView) findViewById(R.id.textView75);
+        TextView myPhone3 = (TextView) findViewById(R.id.textView79);
+        TextView myPhone4 = (TextView) findViewById(R.id.textView81);
+        TextView myPhone5 = (TextView) findViewById(R.id.textView83);
+        TextView myPhone6 = (TextView) findViewById(R.id.textView85);
+        TextView myPhone7 = (TextView) findViewById(R.id.textView87);
+        TextView myPhone8 = (TextView) findViewById(R.id.textView89);
+        TextView myPhone9 = (TextView) findViewById(R.id.textView92);
+        TextView myPhone10 = (TextView) findViewById(R.id.textView94);
+        TextView myPhone11 = (TextView) findViewById(R.id.textView94);
+        TextView myPhone12 = (TextView) findViewById(R.id.textView96);
+        TextView myPhone13 = (TextView) findViewById(R.id.textView98);
+        TextView myPhone14 = (TextView) findViewById(R.id.textView100);
+        TextView myPhone15 = (TextView) findViewById(R.id.textView102);
+        TextView myPhone16 = (TextView) findViewById(R.id.textView104);
+        TextView myPhone17 = (TextView) findViewById(R.id.textView106);
+        TextView myPhone18 = (TextView) findViewById(R.id.textView108);
+        TextView myPhone19 = (TextView) findViewById(R.id.textView110);
+        TextView myPhone20 = (TextView) findViewById(R.id.textView112);
+        TextView myPhone21 = (TextView) findViewById(R.id.textView114);
+        TextView myPhone22 = (TextView) findViewById(R.id.textView116);
+        TextView myPhone23 = (TextView) findViewById(R.id.textView118);
+        TextView myPhone24 = (TextView) findViewById(R.id.textView120);
+        TextView myPhone25 = (TextView) findViewById(R.id.textView122);
+        TextView myPhone26 = (TextView) findViewById(R.id.textView124);
+        TextView myPhone27 = (TextView) findViewById(R.id.textView126);
+        TextView myPhone28 = (TextView) findViewById(R.id.textView128);
+        TextView myPhone29 = (TextView) findViewById(R.id.textView130);
+        TextView myPhone30 = (TextView) findViewById(R.id.textView132);
+        TextView myPhone31 = (TextView) findViewById(R.id.textView134);
+        TextView myPhone32 = (TextView) findViewById(R.id.textView136);
+        TextView myPhone33 = (TextView) findViewById(R.id.textView138);
+        TextView myPhone34 = (TextView) findViewById(R.id.textView140);
+        TextView myPhone35 = (TextView) findViewById(R.id.textView142);
+        TextView myPhone36 = (TextView) findViewById(R.id.textView144);
+        TextView myPhone37 = (TextView) findViewById(R.id.textView146);
+        TextView myPhone38 = (TextView) findViewById(R.id.textView148);
+        TextView myPhone39 = (TextView) findViewById(R.id.textView150);
+        TextView myPhone40 = (TextView) findViewById(R.id.textView152);
+        TextView myPhone41 = (TextView) findViewById(R.id.textView154);
+
+        SpannableString content = new SpannableString("Content");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        myPhone.setText(content);
+
 //
 //
 //
@@ -32,7 +83,426 @@ public class AdministrativeNumbers extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        myPhone.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-2407102"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 04942407150"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7103"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7104"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7106"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7105"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7108"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone7.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7107"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone8.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7142"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone9.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7109"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone10.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7112"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone11.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7110"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone12.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7111"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone13.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7123"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone14.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7114"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone15.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7133"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone16.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7125"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone17.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7127"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone18.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7116"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone19.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7115"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone20.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7209"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone21.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7124"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone22.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7121"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone23.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7119"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone24.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7126"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone25.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7122"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone26.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7136"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone27.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7100"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone28.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7132"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone29.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7131"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone30.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7128"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone31.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7138"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone32.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7130"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone33.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7117"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone34.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7444"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone35.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7316"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone36.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7500"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone37.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7401"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone38.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7546"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone39.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7233"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone40.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7227"));
+                startActivity(callIntent);
+            }
+        });
+        myPhone41.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel: 0494-240-7152"));
+                startActivity(callIntent);
+            }
+        });
 
     }
 
--- app/src/main/java/in/gravitykerala/universityofcalicut/TalktoVC.java
+++ app/src/main/java/in/gravitykerala/universityofcalicut/TalktoVC.java
@@ -1,5 +1,7 @@
 package in.gravitykerala.universityofcalicut;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,8 +10,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+
+import java.net.MalformedURLException;
+
+import in.gravitykerala.universityofcalicut.Models.MobileVcEnqeryDTO;
 
 
 public class TalktoVC extends AppCompatActivity {
@@ -18,7 +28,11 @@ public class TalktoVC extends AppCompatActivity {
     Button submit;
     String MobilePattern = "[0-9]{10}";
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private MobileServiceClient mClient;
 
+    private ProgressBar mProgressBar;
+    private MobileServiceTable<MobileVcEnqeryDTO> mToDoTable;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +41,17 @@ public class TalktoVC extends AppCompatActivity {
         if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
+        try {
+            // Create the Mobile Service Client instance, using the provided
+            // Mobile Service URL and key
+            mClient = new MobileServiceClient(
+                    "https://universityofcalicut.azure-mobile.net/",
+                    "XWXXhaCoiYqzzERpfsqnhpJuQBgCAw42",
+                    this);
+
+            // Get the Mobile Service Table instance to use
+            mToDoTable = mClient.getTable(MobileVcEnqeryDTO.class);
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         name = (EditText) findViewById(R.id.name);
         final String name_s = name.toString();
@@ -38,9 +63,25 @@ public class TalktoVC extends AppCompatActivity {
         final String phn_s = phn.toString();
         submit = (Button) findViewById(R.id.button_submit);
         spinner = (Spinner) findViewById(R.id.spinner);
+        } catch (MalformedURLException e) {
+            //createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
+        }
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if (mClient == null) {
+                    return;
+                }
+
+                // Create a new item
+                final MobileVcEnqeryDTO item = new MobileVcEnqeryDTO();
+
+                item.uName = (name.getText().toString());
+                item.Phno = (phn.getText().toString());
+                item.Email = (email.getText().toString());
+                item.content = (messages.getText().toString());
+                item.enquiry = (spinner.getSelectedItem().toString());
                 if (name.getText().toString().isEmpty() || email.getText().toString().isEmpty() || phn.getText().toString().isEmpty() || messages.getText().toString().isEmpty()) {
                     Toast.makeText(TalktoVC.this, "fill all the fields", Toast.LENGTH_SHORT).show();
 
@@ -49,9 +90,37 @@ public class TalktoVC extends AppCompatActivity {
                 } else if (!phn.getText().toString().matches(MobilePattern)) {
                     Toast.makeText(getApplicationContext(), "Please enter valid number", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Submitted", Toast.LENGTH_SHORT).show();
-                }
 
+
+                    // item.setComplete(false);
+
+                    // Insert the new item
+                    new AsyncTask<Void, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Void... params) {
+                            try {
+                                final MobileVcEnqeryDTO entity = mToDoTable.insert(item).get();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(TalktoVC.this, "THANK YOU", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            } catch (Exception e) {
+                                // createAndShowDialog(e, "Error");
+
+                            }
+
+                            return null;
+                        }
+                    }.execute();
+
+                    name.setText("");
+                    phn.setText("");
+                    email.setText("");
+                    messages.setText("");
+                    spinner.setSelection(0);
+                }
 
             }
         });
